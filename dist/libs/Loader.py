445d49f14d191effb44b918416e38772e38fcf20
Shanda Lau <shandalaulv@gmail.com>
2020-12-08 21:23:04 +0800

    add sort by int.

--- dist/libs/Loader.py
+++ dist/libs/Loader.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import torch.nn as nn
+import re
 from torch.autograd import Variable
 
 def is_image_file(filename):
@@ -47,14 +48,34 @@ class Dataset(data.Dataset):
     def __len__(self):
         return len(self.image_list)
 
+def tryint(s):  # 将元素中的数字转换为int后再排序
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+
+def str2int(v_str):  # 将元素中的字符串和数字分割开
+    return [tryint(sub_str) for sub_str in re.split('([0-9]+)', v_str)]
+
+
+def sort_humanly(v_list):  # 以分割后的list为单位进行排序
+    """
+    sort list strings according string and number
+    :param v_list:
+    :return:
+    """
+    return sorted(v_list, key=str2int, reverse=False)
+
 class Dataset_Video(data.Dataset):
     def __init__(self,dataPath,loadSize, fineSizeH, fineSizeW, test=False,video=False):
         super(Dataset_Video,self).__init__()
         self.dataPath = dataPath
         self.image_list = [x for x in os.listdir(dataPath) if is_image_file(x) and os.path.basename !='preview.png']
-        self.image_list = sorted(self.image_list)
+        self.image_list = sort_humanly(self.image_list)
+        print(self.image_list)
         if(video):
-            self.image_list = sorted(self.image_list)
+            self.image_list = sort_humanly(self.image_list)
         if not test:
             self.transform = transforms.Compose([
             		         transforms.Resize((fineSizeH, fineSizeW)),
